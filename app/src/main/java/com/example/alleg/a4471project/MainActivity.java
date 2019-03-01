182559a43cb65951ccd899b2939b22ca855fac9f
SnackKing <allegretti813@gmail.com>
2019-03-01 14:21:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alleg/a4471project/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alleg.a4471project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
