c338b6a033923999c250a34c9f82f6cf2bd73adf
SnackKing <allegretti813@gmail.com>
2019-03-11 15:15:52 -0400

    Implemented contact reading and pushing to firebase when app is launched (wiring up Max's code)

--- app/src/main/java/com/example/alleg/a4471project/MainActivity.java
+++ app/src/main/java/com/example/alleg/a4471project/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.alleg.a4471project;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private final int PERMISSIONS = 1;
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkPermissions();
         setContentView(R.layout.activity_main);
+        checkPermissions();
+        getContactList();
     }
     public void checkPermissions(){
         String[] requests = {
@@ -50,5 +61,59 @@ public class MainActivity extends AppCompatActivity {
             // permissions this app might request.
         }
     }
+    private void writeNewContact(String userId, String fullName, String phoneNumber){
+        Contact contact = new Contact(fullName, phoneNumber);
+
+        mRootRef.child("contacts").child(userId).child(fullName).setValue(contact);
+    }
+
+    private void writeNewLocation(String userId, String longitude, String latitude){
+        Location location = new Location(longitude, latitude);
+
+        mRootRef.child("locations").child(userId).setValue(location);
+    }
+
+    private void writeNewCallLog(String userId, String opposingNumber, LocalDateTime callStart, LocalDateTime callEnd){
+        Call_Log callLog = new Call_Log(opposingNumber, callStart, callEnd);
+
+        mRootRef.child("call_log").child(userId).setValue(callLog);
+    }
+    private void getContactList() {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+            while (cur != null && cur.moveToNext()) {
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+
+                if (cur.getInt(cur.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        String userID = getUserid();
+                        writeNewContact(userID, name, phoneNo);
+                    }
+                    pCur.close();
+                }
+            }
+        }
+        if(cur!=null){
+            cur.close();
+        }
+    }
+    public String getUserid(){
+        return FirebaseAuth.getInstance().getUid();
+    }
+
 
 }
