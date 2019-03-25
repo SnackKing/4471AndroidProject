6c14bab3b19b33c98d4478c2dc8b8cb754122615
SnackKing <allegretti813@gmail.com>
2019-03-25 14:32:22 -0400

    Added id field to user

--- app/src/main/java/com/example/alleg/a4471project/User.java
+++ app/src/main/java/com/example/alleg/a4471project/User.java
@@ -8,15 +8,17 @@ public class User {
     public String full_name;
     public String phone_number;
     public LocalDateTime join_date;
+    public String user_id;
 
     public User(){
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String dateOfBirth, String fullName, String phoneNumber, LocalDateTime joinDate){
+    public User(String dateOfBirth, String fullName, String phoneNumber, LocalDateTime joinDate, String uid){
         this.date_of_birth = dateOfBirth;
         this.full_name = fullName;
         this.phone_number = phoneNumber;
         this.join_date = joinDate;
+        this.user_id = uid;
     }
 }
